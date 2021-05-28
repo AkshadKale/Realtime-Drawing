90ddb8caaa42670b4d78d3db099e88c0dac04424
AkshadKale <74201779+AkshadKale@users.noreply.github.com>
2021-05-28 22:18:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+right_WristX = 0 ;
+left_WristX = 0;
+difference = 0 ;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -9,7 +15,11 @@ function setup(){
 }
 
 function draw(){
-    background('#969A97')
+    background('#969A97');
+    document.getElementById("square_side").innerHTML = "Width and Height of a Square will be = " + difference + "px";
+    fill('#90093');
+    stroke('#90093');
+    square(noseX , noseY , difference);
 }
 
 function modelLoaded(){
@@ -20,6 +30,18 @@ function gotPoses(results)
 { 
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + ", noseY = " + noseY);
+
+        left_WristX = results[0].pose.leftWrist.x;
+        right_WristX = results[0].pose.rightWrist.x;
+
+        difference = floor(left_WristX - right_WristX);
+
+        console.log("right Wrist = " + right_WristX + " left wrist " + left_WristX + " difference " + difference);
+
     }
 
 }
+
